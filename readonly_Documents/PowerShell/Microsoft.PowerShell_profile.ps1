d4f71cbe1ac844c717055e875582f208238a3a66
kot149 <131101078+kot149@users.noreply.github.com>
2025-03-30 19:44:39 +0900

    Add PowerShell profile

---
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,56 @@
+###################################
+# Oh-My-Posh
+###################################
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\mytheme.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\mytheme2.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\hunk.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\montys.omp.json" | Invoke-Expression
+
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+# Add cwd to PATH
+if(-not $env:path.Split(';').Contains('.')){
+    $env:path += ";."
+}
+
+###################################
+# Modules
+###################################
+Import-Module syntax-highlighting
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
+
+# Import-Module -Name Terminal-Icons
+
+
+###################################
+# Aleases
+###################################
+Set-Alias touch New-Item
+Set-Alias ls Get-ChildItem
+Set-Alias ll Get-ChildItem -Force
+Set-Alias cat Get-Content
+# New-Alias -Name grep -Value Select-String
+
+function git-logout {
+	cmdkey /delete:git:https://github.com
+}
+
+function export() {
+    ($key, $value) = $args[0] -split "=";
+    set-item "env:${key}" $value;
+}
+
+function unset() {
+    $key = $args[0];
+    remove-item "env:${key}";
+}
+
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+If (Test-Path "C:\Users\Ku0143\anaconda3\Scripts\conda.exe") {
+    (& "C:\Users\Ku0143\anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+}
+#endregion
