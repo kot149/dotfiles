b0077a487e8d1e4c40068b6ccdee876abca45569
kot149 <131101078+kot149@users.noreply.github.com>
2026-03-20 19:14:21 +0900

    Set gnome terminal default shell to zsh

--- private_dot_config/home-manager/linux.nix
+++ private_dot_config/home-manager/linux.nix
@@ -3,4 +3,13 @@
 {
   home.packages = with pkgs; [
   ];
+
+  programs.gnome-terminal.enable = true;
+
+  programs.gnome-terminal.profile."b19a5eec-d2dc-43d7-b4e5-c4f8c183fd34" = {
+    default = true;
+    visibleName = "default";
+    customCommand = "${pkgs.zsh}/bin/zsh";
+    loginShell = true;
+  };
 }
