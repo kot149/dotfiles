a3a64de16b334d66d3588039dcb546001019a37b
kot149 <131101078+kot149@users.noreply.github.com>
2026-04-30 23:44:51 +0900

    feat(nix): Add configuration for ghostty on WSLg

---
+++ private_dot_config/home-manager/wsl.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+
+let
+  nixGlSrc = pkgs.fetchFromGitHub {
+    owner = "nix-community";
+    repo = "nixGL";
+    rev = "b6105297e6f0cd041670c3e8628394d4ee247ed5";
+    sha256 = "sha256-fbRQzIGPkjZa83MowjbD2ALaJf9y6KMDdJBQMKFeY/8=";
+  };
+  nixGl = import nixGlSrc {
+    pkgs = pkgs;
+    enable32bits = false;
+    enableIntelX86Extensions = pkgs.stdenv.isLinux && pkgs.stdenv.isx86_64;
+  };
+  ghosttyWrapped = pkgs.writeShellScriptBin "ghostty" ''
+    exec ${nixGl.nixGLIntel}/bin/nixGLIntel ${pkgs.ghostty}/bin/ghostty "$@"
+  '';
+in
+{
+  home.packages = [ ghosttyWrapped ];
+
+  programs.ghostty = {
+    enable = true;
+    package = ghosttyWrapped;
+  };
+}
