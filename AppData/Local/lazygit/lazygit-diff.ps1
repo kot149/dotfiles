744417a7f626f8b0a6ae1885bd6570c24275d11c
kot149 <131101078+kot149@users.noreply.github.com>
2026-07-08 11:37:34 +0900

    feat(git,lazygit): replace delta pager with hunk

--- AppData/Local/lazygit/lazygit-diff.ps1
+++ AppData/Local/lazygit/lazygit-diff.ps1
@@ -10,11 +10,4 @@ $old = $args[1].Replace('\', '/')
 $new = $args[4].Replace('\', '/')
 $path = $args[0]
 
-$deltaArgs = @()
-if ($env:LAZYGIT_COLUMNS -and $env:LAZYGIT_COLUMNS.Trim() -ne '') {
-  $deltaArgs += "--width=$($env:LAZYGIT_COLUMNS.Trim())"
-}
-
-git -c core.pager=cat diff --no-index --no-ext-diff -- $old $new |
-  ForEach-Object { $_.Replace($old, $path).Replace($new, $path) } |
-  & delta @deltaArgs
+& hunk difftool $old $new $path --mode split
